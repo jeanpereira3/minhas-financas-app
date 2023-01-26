1357438c1378471fc09b597f7894e666587ba5c2
jeanpereira3 <jeansilv13@gmail.com>
2023-01-26 13:34:43 -0300

    adicionado funcao para recuperar saldo

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,15 +1,29 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 
 import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 const Home = () => {
+
+  const [saldo, setSaldo] = useState(0)
+
+  useEffect(()=>{
+    axios.get('http://localhost:8080/api/usuarios/494/saldo')
+      .then(response => {
+        setSaldo(response.data)
+      }).catch(erro => {
+        console.log(erro.response)
+      })
+  },[])
+
   return (
     <div className='container'>
 
       <div className='jumbotron'>
         <h1 className='display-3'>Bem vindo!</h1>
         <p className='lead'>Esse é seu sistema de finanças.</p>
-        <p className='lead'>Seu saldo para o mês atual é de R$ 5.325,21</p>
+        <p className='lead'>Seu saldo para o mês atual é de R$ {saldo}</p>
         <hr className='my-4' />
         <p>E essa é sua área administrativa, utilize um dos menus ou botões abaixo para navegar pelo sistema.</p>
         <p className='lead'>
