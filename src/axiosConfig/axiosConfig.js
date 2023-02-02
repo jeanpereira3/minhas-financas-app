7f4f3e305d99e3aac8517a9896ba2627c2d0b4d7
jeanpereira3 <jeansilv13@gmail.com>
2023-02-02 16:40:11 -0300

    adicionado url para api

--- src/axiosConfig/axiosConfig.js
+++ src/axiosConfig/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const httpClient = axios.create({
-  baseURL: 'http://localhost:8080'
+  baseURL: 'minhas-financas-production-d5c5.up.railway.app'
 })
 
 export default httpClient
